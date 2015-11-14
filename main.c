228855cbe3eaa74167a04034fc4fbe339e58b195
Diego Silva <die6o.silva@gmail.com>
2015-11-14 16:57:06 -0200

    primeira versao

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Diego
+ *
+ * Created on 14 de Novembro de 2015, 16:51
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
